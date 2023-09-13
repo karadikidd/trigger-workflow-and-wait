9af75e6d02fe606f3b03d7e832e8824d0a9311e3
Victor Shvartsman <victor.shvartsman@cardinalops.com>
2023-09-13 09:17:17 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -151,8 +151,8 @@ wait_for_workflow_to_finish() {
   echo "Waiting for workflow to finish:"
   echo "The workflow id is [${last_workflow_id}]."
   echo "The workflow logs can be found at ${last_workflow_url}"
-  echo "::set-output name=workflow_id::${last_workflow_id}"
-  echo "::set-output name=workflow_url::${last_workflow_url}"
+  echo "workflow_id=${last_workflow_id}" >> $GITHUB_OUTPUT
+  echo "workflow_url=${last_workflow_url}" >> $GITHUB_OUTPUT
   echo ""
 
   conclusion=null
